ea3388bf60ef7cade589fcd18124fd9e5db6bf8f
Javier <jrzaurin@gmail.com>
2018-09-11 17:28:34 +0100

    adding script on how to use ncdg

---
+++ Ponpare/py_scripts/using_ncdg.py
@@ -0,0 +1,143 @@
+import numpy as np
+import random
+
+# let's build a fake example to illustrate how one could use the normalised
+# discounted cumulative gain (ndcg) for the excercise here.
+
+# To use this metric one needs an estimate of the real relevance of the items
+# we recommend. In our case, this would be the interest.
+
+# As it happened before, there is a caveat worth mentioning here. We are
+# recommending a few hundred coupons while the users only interacted with a
+# few. Therefore, it is not possible to know/compute the real relevance for
+# all recommended items. there are 2 ways to go about it:
+
+# 1-We can rank based on interest, keep the top 10 and compute the metric with
+# the intersect between those 10 and the ones that the user interacted with, or
+
+# 2-We can extract all the coupons that the user interacted with, with their
+# rank. If there are more than 10, we cut at 10, if there are less, we
+# proceed.
+
+# We will implement both methods
+
+# let's assume we have 10 users
+user_ids = ['user_'+str(i) for i in 1+np.arange(10)]
+
+# and we recommend 50 coupons
+recommended_coupon_ids = ['coupon_'+str(i) for i in 1+np.arange(50)]
+
+# each user interacted with a number of coupons (between 1-50) during testing
+random.seed(1)
+real_interactions = {}
+for user in user_ids:
+	n_coupons = random.randint(1,50)
+	coupon_ids = random.sample(recommended_coupon_ids, n_coupons)
+	interest = [random.random() for _ in range(len(coupon_ids))]
+	real_interactions[user] = dict(zip(coupon_ids,interest))
+
+random.seed(2)
+recommendations = {}
+for user in user_ids:
+	interest = [random.random() for _ in range(len(recommended_coupon_ids))]
+	recommendations[user] = dict(zip(recommended_coupon_ids,interest))
+
+# In reality what we need to evaluate is a list of dictionaries
+real_interactions = [v for v in real_interactions.values()]
+recommendations = [v for v in recommendations.values()]
+
+def dcgk(actual, predicted, method=1, k=10):
+    """
+    Computes the discounted cumulative gain at k.
+    This function computes the discounted cumulative gain at k between two
+    dictionaries of items: scores
+
+    Parameters
+    ----------
+    actual : dict
+             A dict of elements: {'item': score} for the actual interactions
+    predicted : dict
+                A dict of predicted elements {'item': score}
+    k : int, optional
+        The maximum number of predicted elements
+	method: int
+        1: sort the recommendations and cut at k. Then intersect with actual
+        2: sort the recommendations and intersect with actual. Then cut at min(len(actual), k)
+
+    Returns
+    -------
+    score : double
+            discounted cumulative gain at k
+    """
+	if method==1:
+		# First rank the recommendations based on predicted interest and keep the top 10
+		ranked_rec = sorted([(k,v) for k,v in predicted.items()],
+			key=lambda x: x[1], reverse=True)[:k]
+		# Among these top 10 keep those that the user did interact with and their rank
+		ranked_rec = [ (i+1,k) for i,(k,v) in enumerate(ranked_rec) if k in actual.keys()]
+		# then extract the rank and the relevance (real interest)
+		ranked_scores = [(k,actual[v]) for k,v in dict(ranked_rec).items()]
+	elif method==2:
+		# First rank the recommendations based on predicted interest
+		ranked_rec = sorted([(k,v) for k,v in predicted.items()],
+			key=lambda x: x[1], reverse=True)
+		# Select those that the user did interact with and their rank
+		ranked_rec = [ (i+1,k) for i,(k,v) in enumerate(ranked_rec) if k in actual.keys()]
+		# then extract the rank and the relevance (real interest) and keep min(len(actual), k)
+		ranked_scores = [(k,actual[v]) for k,v in dict(ranked_rec).items()][:min(len(actual), k)]
+
+	if not ranked_scores:
+		return 0.
+	else:
+		rank = np.asarray([s[0] for s in ranked_scores])
+		score = np.asarray([s[1] for s in ranked_scores])
+		return np.sum((2**score - 1) / np.log2(rank+1))
+
+def ndcgk(actual, predicted, method=1, k=10):
+    """Normalized discounted cumulative gain (NDCG) at rank K.
+
+    Parameters
+    ----------
+    actual : dict
+             A dict of elements: {'item': score} for the actual interactions
+    predicted : dict
+                A dict of predicted elements {'item': score}
+    k : int, optional
+        The maximum number of predicted elements
+	method: int
+        1: sort the recommendations and cut at k. Then intersect with actual
+        2: sort the recommendations and intersect with actual. Then cut at min(len(actual), k)
+
+    Returns
+    -------
+    score : double
+            normalised discounted cumulative gain at k
+    """
+	return dcgk(actual,predicted) / dcgk(actual,actual)
+
+
+def mndcgk(actual, predicted, method=1, k=10):
+	"""
+	Normalized discounted cumulative gain (NDCG) at rank K over all recommendations
+    Parameters
+    ----------
+    actual : dict
+             A dict of elements: {'item': score} for the actual interactions
+    predicted : dict
+                A dict of predicted elements {'item': score}
+    k : int, optional
+        The maximum number of predicted elements
+	method: int
+        1: sort the recommendations and cut at k. Then intersect with actual
+        2: sort the recommendations and intersect with actual. Then cut at min(len(actual), k)
+
+    Returns
+    -------
+    score : double
+            mean normalised discounted cumulative gain at k
+    """
+	return np.mean([ndcgk(a,p,method,k) for a,p in zip(actual,predicted)])
+
+
+
+
