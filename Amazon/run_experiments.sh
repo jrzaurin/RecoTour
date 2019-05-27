c25b04e04b8a0ac9809145b31176d247c8caf499
jrzaurin <jrzaurin@gmail.com>
2019-05-27 12:11:03 +0100

    summary of the results almost done

--- Amazon/run_experiments.sh
+++ Amazon/run_experiments.sh
@@ -1,38 +1,45 @@
-# time python gmf.py --batch_size 512 --lr 0.01 --n_emb 8 --epochs 30
-# time python gmf.py --batch_size 1024 --lr 0.01 --n_emb 8 --epochs 30
+python gmf.py --batch_size 512 --lr 0.01 --n_emb 8 --epochs 30
+python gmf.py --batch_size 1024 --lr 0.01 --n_emb 8 --epochs 30
 
-# time python gmf.py --batch_size 512 --lr 0.01 --n_emb 16 --epochs 30 --validate_every 2
+python gmf.py --batch_size 512 --lr 0.01 --n_emb 16 --epochs 30 --validate_every 2
 
-# time python gmf.py --batch_size 512 --lr 0.01 --lr_scheduler --learner "SGD" --n_emb 8 --epochs 30
-# time python gmf.py --batch_size 512 --lr 0.01 --lr_scheduler --learner "RMSprop" --n_emb 8 --epochs 30
-# time python gmf.py --batch_size 512 --lr 0.01 --lr_scheduler --n_emb 8 --epochs 30
+python gmf.py --batch_size 512 --lr 0.01 --lr_scheduler --learner "SGD" --n_emb 8 --epochs 30
+python gmf.py --batch_size 512 --lr 0.01 --lr_scheduler --learner "RMSprop" --n_emb 8 --epochs 30
+python gmf.py --batch_size 512 --lr 0.01 --lr_scheduler --n_emb 8 --epochs 30
 
-time python gmf.py --batch_size 512 --lr 0.001 --n_emb 64 --epochs 30
+python gmf.py --batch_size 512 --lr 0.001 --n_emb 64 --epochs 30
 
-time python mlp.py --batch_size 512 --lr 0.01 --layers "[32, 16, 8]" --epochs 30
-time python mlp.py --batch_size 512 --lr 0.01 --layers "[64, 32, 16]" --epochs 30
-time python mlp.py --batch_size 512 --lr 0.01 --layers "[128, 64, 32]" --epochs 30
-time python mlp.py --batch_size 512 --lr 0.01 --layers "[256, 128, 64]" --epochs 30
+python mlp.py --batch_size 512 --lr 0.01 --layers "[32, 16, 8]" --epochs 30
+python mlp.py --batch_size 512 --lr 0.01 --layers "[64, 32, 16]" --epochs 30
+python mlp.py --batch_size 512 --lr 0.01 --layers "[128, 64, 32]" --epochs 30
+python mlp.py --batch_size 512 --lr 0.01 --layers "[256, 128, 64]" --epochs 30
 
-# python mlp.py --batch_size 512 --lr 0.01 --layers "[64, 32, 16]" --dropouts "[0.25, 0.25]" --epochs 40
-# python mlp.py --batch_size 512 --lr 0.01 --layers "[128, 64, 32]" --dropouts "[0.25, 0.25]" --epochs 40
-# python mlp.py --batch_size 512 --lr 0.01 --layers "[256, 128, 64]" --dropouts "[0.25, 0.25]" --epochs 40
+python gmf.py --batch_size 512 --lr 0.01 --n_emb 64 --epochs 30
+python gmf.py --batch_size 512 --lr 0.03 --n_emb 64 --lr_scheduler --epochs 30
+python gmf.py --batch_size 512 --lr 0.03 --n_emb 64 --epochs 30
 
-# python neumf.py --batch_size 256 --lr 0.01 --n_emb 64 --layers "[256, 128, 64]" --dropouts "[0.,0.]" --epochs 30
-# python neumf.py --batch_size 256 --lr 0.001 --n_emb 64 --layers "[256, 128, 64]" --dropouts "[0.,0.]" \
-# --mf_pretrain "GMF_bs_256_lr_001_n_emb_64.pt" \
-# --mlp_pretrain "MLP_bs_256_reg_00_lr_001_n_emb_128_ll_64_dp_wodp.pt" \
-# --learner "SGD" --epochs 10
-# python neumf.py --batch_size 256 --lr 0.001 --n_emb 64 --layers "[256, 128, 64]" --dropouts "[0.,0.]" \
-# --mf_pretrain "GMF_bs_256_lr_001_n_emb_64.pt" \
-# --mlp_pretrain "MLP_bs_256_reg_00_lr_001_n_emb_128_ll_64_dp_wodp.pt" \
-# --freeze 1 --learner "SGD" --epochs 5
+python mlp.py --batch_size 512 --lr 0.03 --layers "[32, 16, 8]" --epochs 30
+python mlp.py --batch_size 512 --lr 0.03 --layers "[64, 32, 16]" --epochs 30
+python mlp.py --batch_size 512 --lr 0.03 --layers "[128, 64, 32]" --epochs 30
+python mlp.py --batch_size 512 --lr 0.03 --layers "[256, 128, 64]" --epochs 30
 
-# python neumf.py --batch_size 256 --lr 0.01 --n_emb 64 --layers "[256, 128, 64]" --dropouts "[0.,0.]" \
-# --mf_pretrain "GMF_bs_256_lr_001_n_emb_64.pt" \
-# --mlp_pretrain "MLP_bs_256_reg_00_lr_001_n_emb_128_ll_64_dp_wodp.pt" \
-# --epochs 20
-# python neumf.py --batch_size 256 --lr 0.01 --n_emb 64 --layers "[256, 128, 64]" --dropouts "[0.,0.]" \
-# --mf_pretrain "GMF_bs_256_lr_001_n_emb_64.pt" \
-# --mlp_pretrain "MLP_bs_256_reg_00_lr_001_n_emb_128_ll_64_dp_wodp.pt" \
-# --freeze 1 --epochs 20
+python gmf.py --batch_size 512 --lr 0.03 --n_emb 8 --epochs 30
+python gmf.py --batch_size 512 --lr 0.03 --n_emb 16 --epochs 30
+python gmf.py --batch_size 512 --lr 0.03 --n_emb 32 --epochs 30
+
+python gmf.py --batch_size 512 --lr 0.03 --n_emb 32 --epochs 30
+
+python neumf.py --batch_size 512 --lr 0.03 --lr_scheduler --n_emb 32 --layers "[128, 64, 32]" --dropouts "[0.,0.]" \
+--mf_pretrain "GMF_bs_512_lr_003_n_emb_32_lrnr_adam_lrs_wolrs.pt" \
+--mlp_pretrain "MLP_bs_512_reg_00_lr_003_n_emb_64_ll_32_dp_wodp_lrnr_adam_lrs_wolrs.pt" \
+--epochs 20
+
+python neumf.py --batch_size 512 --lr 0.003 --n_emb 32 --layers "[128, 64, 32]" --dropouts "[0.,0.]" \
+--mf_pretrain "GMF_bs_512_lr_003_n_emb_32_lrnr_adam_lrs_wolrs.pt" \
+--mlp_pretrain "MLP_bs_512_reg_00_lr_003_n_emb_64_ll_32_dp_wodp_lrnr_adam_lrs_wolrs.pt" \
+--freeze 1 --epochs 6
+
+python neumf.py --batch_size 512 --lr 0.01 --lr_scheduler --n_emb 32 --layers "[128, 64, 32]" --dropouts "[0.,0.]" \
+--mf_pretrain "GMF_bs_512_lr_003_n_emb_32_lrnr_adam_lrs_wolrs.pt" \
+--mlp_pretrain "MLP_bs_512_reg_00_lr_003_n_emb_64_ll_32_dp_wodp_lrnr_adam_lrs_wolrs.pt" \
+--freeze 1 --epochs 6
