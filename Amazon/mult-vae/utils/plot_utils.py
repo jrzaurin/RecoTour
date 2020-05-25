f1dbfe50f2928b08dc1497fca95c02e965b81b43
jrzaurin <jrzaurin@gmail.com>
2020-05-25 15:50:39 +0100

    waiting for experiments to finish on the p2. All ready to publish post

--- Amazon/mult-vae/utils/plot_utils.py
+++ Amazon/mult-vae/utils/plot_utils.py
@@ -1,7 +1,7 @@
 import pickle
 from glob import glob
 from pathlib import Path
-from typing import Dict, Tuple
+from typing import Dict, List, Tuple
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -16,7 +16,7 @@ results_dir = Path(
 )
 
 
-def prepare_dataframe_for_plot(
+def prepare_dataframe_for_anneal_schedule_plot(
     dataset: str, dl_frame: str
 ) -> Tuple[Dict, pd.DataFrame]:
     """
@@ -84,12 +84,14 @@ def plot_anneal_schedule_dataset(dataset: str):
     def anneal2epoch(x):
         return x * 170
 
-    pt_best_anneal_epochs, pt_df_anneal_schedules = prepare_dataframe_for_plot(
-        dataset, "pt"
-    )
-    mx_best_anneal_epochs, mx_df_anneal_schedules = prepare_dataframe_for_plot(
-        dataset, "mx"
-    )
+    (
+        pt_best_anneal_epochs,
+        pt_df_anneal_schedules,
+    ) = prepare_dataframe_for_anneal_schedule_plot(dataset, "pt")
+    (
+        mx_best_anneal_epochs,
+        mx_df_anneal_schedules,
+    ) = prepare_dataframe_for_anneal_schedule_plot(dataset, "mx")
 
     plt.figure(figsize=(15, 4))
 
@@ -143,6 +145,7 @@ def find_best(dl_frame: str, model: str) -> pd.DataFrame:
         "p_dims",
         "weight_decay",
         "lr",
+        "lr_scheduler",
         "anneal_cap",
         "best_epoch",
         "loss",
@@ -179,16 +182,24 @@ def find_best(dl_frame: str, model: str) -> pd.DataFrame:
     )
     results_df["dl_frame"] = "Pytorch" if dl_frame == "pt" else "Mxnet"
     results_df.loc[results_df.model == "dae", "anneal_cap"] = "NA"
-    return results_df[keep_cols]
+
+    return results_df[keep_cols].round(3)
 
 
-def plot_dataset_loss(df: pd.DataFrame, dataset: str):
+def plot_dataset_loss(dataset: str):
+
+    df = all_results_df()
+
     df_pt = df[(df.dataset == dataset) & (df.dl_frame == "Pytorch")][
         ["loss", "n100", "r20", "r50"]
     ].melt("loss", var_name="metric", value_name="value")
     df_mx = df[(df.dataset == dataset) & (df.dl_frame == "Mxnet")][
         ["loss", "n100", "r20", "r50"]
     ].melt("loss", var_name="metric", value_name="value")
+    # manually removing an "odd" mxnet ml_experiment
+    if dataset == "movielens":
+        df_mx = df_mx.sort_values('loss')
+        df_mx.drop(df_mx.tail(3).index,inplace=True)
 
     plt.figure(figsize=(15, 4))
 
@@ -230,6 +241,159 @@ def plot_dataset_loss(df: pd.DataFrame, dataset: str):
     plt.subplots_adjust(top=0.8)
 
 
-def plot_loss(df: pd.DataFrame):
-    plot_dataset_loss(df, "movielens")
-    plot_dataset_loss(df, "amazon")
+def plot_metric_vs_loss():
+    plot_dataset_loss("movielens")
+    plot_dataset_loss("amazon")
+
+
+def plot_ndcg_vs_pdims():
+    def get_dae_experiments(dataset: str, keep_keys: List[str]):
+        dataset_res = []
+        for f, r in zip(model_files, results):
+            if (
+                (r["args"]["dataset"] == dataset)
+                & (r["args"]["lr"] == 0.001)
+                & (len(eval(r["args"]["p_dims"])) == 2)
+                & (r["args"]["weight_decay"] == 0.0)
+            ):
+                r["args"]["dl_frame"] = f.split("/")[-1].split("_")[0]
+                r["args"]["nunits"] = eval(r["args"]["p_dims"])[0]
+                out_r = {k: v for k, v in r["args"].items() if k in keep_keys}
+                out_r["n100"] = r["n100"]
+                dataset_res.append(out_r)
+        return dataset_res
+
+    keep_keys = [
+        "dataset",
+        "dl_frame",
+        "nunits",
+        "n100",
+    ]
+
+    pattern = "*dae*"
+    search_dir = results_dir / pattern
+
+    model_files = glob(str(search_dir))
+
+    results = []
+    for f in model_files:
+        results.append(pickle.load(open(f, "rb")))
+
+    movielens_res = get_dae_experiments("movielens", keep_keys)
+    amazon_res = get_dae_experiments("amazon", keep_keys)
+
+    movielens_df = pd.concat(
+        [pd.DataFrame(d, index=[0]) for d in movielens_res]
+    ).reset_index(drop=True)
+    amazon_df = pd.concat([pd.DataFrame(d, index=[0]) for d in amazon_res]).reset_index(
+        drop=True
+    )
+
+    plt.figure(figsize=(15, 4))
+
+    plt.subplot(1, 2, 1)
+    sns.lineplot(
+        x="nunits",
+        y="n100",
+        linewidth=2,
+        data=movielens_df[movielens_df.dl_frame == "pt"],
+        color="indianred",
+        label="Pytorch",
+        legend="full",
+    )
+    sns.lineplot(
+        x="nunits",
+        y="n100",
+        linewidth=2,
+        data=movielens_df[movielens_df.dl_frame == "mx"],
+        label="Mxnet",
+        legend="full",
+    )
+    plt.xlabel("p dims")
+    plt.title("movielens")
+    plt.xticks((50, 100, 200, 300))
+
+    plt.subplot(1, 2, 2)
+    sns.lineplot(
+        x="nunits",
+        y="n100",
+        linewidth=2,
+        data=amazon_df[amazon_df.dl_frame == "pt"],
+        color="indianred",
+        label="Pytorch",
+        legend="full",
+    )
+    sns.lineplot(
+        x="nunits",
+        y="n100",
+        linewidth=2,
+        data=amazon_df[amazon_df.dl_frame == "mx"],
+        label="Mxnet",
+        legend="full",
+    )
+    plt.xlabel("p dims")
+    plt.title("Amazon")
+    plt.xticks((50, 100, 200, 300))
+
+
+def build_results_df(dl_frame: str, model: str):
+
+    keep_cols = {
+        "dataset",
+        "model",
+        "dl_frame",
+        "p_dims",
+        "dropout_enc",
+        "dropout_dec",
+        "weight_decay",
+        "lr",
+        "batch_size",
+        "anneal_cap",
+        "lr_scheduler",
+        "lr_patience",
+        "early_stop_patience",
+        "best_epoch",
+        "loss",
+        "n100",
+        "r20",
+        "r50",
+    }
+
+    pattern = "_".join([dl_frame, model, "*"])
+    search_dir = results_dir / pattern
+
+    model_files = glob(str(search_dir))
+
+    run_results = []
+    for f in model_files:
+        run_results.append(pickle.load(open(f, "rb")))
+
+    sub_keys = [k for k in run_results[0].keys() if k != "args"]
+
+    run_results_dfs = []
+    for r in run_results:
+        dict1 = r["args"]
+        dict2 = {k: v for k, v in r.items() if k in sub_keys}
+        df1 = pd.DataFrame(dict1, index=[0])
+        df2 = pd.DataFrame(dict2, index=[0])
+        run_results_dfs.append(pd.concat([df1, df2], axis=1))
+
+    results_df = pd.concat(run_results_dfs)
+    results_df["dl_frame"] = "Pytorch" if dl_frame == "pt" else "Mxnet"
+    results_df.loc[results_df.model == "dae", "anneal_cap"] = "NA"
+    results_df = results_df[keep_cols].round(3)
+    return results_df
+
+
+def all_results_df():
+
+    all_results = pd.concat(
+        [
+            build_results_df(dl_frame="pt", model="vae"),
+            build_results_df(dl_frame="pt", model="dae"),
+            build_results_df(dl_frame="mx", model="vae"),
+            build_results_df(dl_frame="mx", model="dae"),
+        ]
+    ).reset_index(drop=True)
+
+    return all_results
