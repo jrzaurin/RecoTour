6771c502c7171fbef2863cc3d931f886664441d0
Javier <jrzaurin@gmail.com>
2024-11-25 10:05:59 +0000

    baseline ranking metrics added

--- pytorch_widedeep_reczoo/experiments/ts_experiments/lgb_ranking_metrics.py
+++ pytorch_widedeep_reczoo/experiments/ts_experiments/lgb_ranking_metrics.py
@@ -2,8 +2,10 @@ import pickle
 from typing import Dict, List, Tuple
 from pathlib import Path
 
+import numpy as np
 import pandas as pd
-import catboost as ctb
+import lightgbm as lgb
+from pytorch_widedeep.utils import LabelEncoder
 
 from rec_tools.constants import (
     DATA_DIR,
@@ -19,9 +21,8 @@ from rec_tools.prepare_experiments.prepare_ts import (
 )
 
 
-def set_ctb_datasets() -> Tuple[ctb.Pool, ctb.Pool]:
+def set_lgb_datasets() -> Tuple[lgb.Dataset, lgb.Dataset]:
     train_df, val_df, cat_cols = experiment_without_feat_engineering()
-
     full_train_df = pd.concat([train_df, val_df], ignore_index=True)
 
     test_df = pd.read_csv(
@@ -33,58 +34,63 @@ def set_ctb_datasets() -> Tuple[ctb.Pool, ctb.Pool]:
     full_train_df = full_train_df[cat_cols + ["rating"]]
     test_df = test_df[cat_cols + ["rating"]]
 
-    X_train = full_train_df.drop(columns=["rating"])
+    encoder = LabelEncoder(columns_to_encode=cat_cols)
+    full_train_df_encoded = encoder.fit_transform(full_train_df)
+    test_df_encoded = encoder.transform(test_df)
+
+    X_train = full_train_df_encoded.drop(columns=["rating"])
     y_train = full_train_df["rating"]
-    X_test = test_df.drop(columns=["rating"])
+    X_test = test_df_encoded.drop(columns=["rating"])
     y_test = test_df["rating"]
 
-    train_data = ctb.Pool(
+    train_data = lgb.Dataset(
         X_train,
         label=y_train,
-        cat_features=cat_cols,
+        categorical_feature=cat_cols,
+        free_raw_data=False,
     )
 
-    test_data = ctb.Pool(
+    test_data = lgb.Dataset(
         X_test,
         label=y_test,
-        cat_features=cat_cols,
+        reference=train_data,
+        free_raw_data=False,
     )
 
     return train_data, test_data
 
 
-def train_ctb_model_and_evaluate_ranking_metrics(
+def train_lgb_model_and_evaluate_ranking_metrics(
     k_values: List[int] = [5, 10, 20]
 ) -> Dict[int, Dict[str, float]]:
 
     with open(
-        Path(RESULTS_DIR) / "results_ctb_with_default_params" / "model.pkl", "rb"
+        Path(RESULTS_DIR) / "results_lgb_with_default_params" / "model.pkl", "rb"
     ) as f:
         model_with_default_params = pickle.load(f)
 
-    results_dir = Path(RESULTS_DIR) / "ctb_ranking_metrics"
+    results_dir = Path(RESULTS_DIR) / "lgb_ranking_metrics"
     results_dir.mkdir(parents=True, exist_ok=True)
 
-    train_data, test_data = set_ctb_datasets()
+    train_data, test_data = set_lgb_datasets()
 
-    model = ctb.train(
-        pool=train_data,
-        params={
-            "loss_function": "Logloss",
-            "iterations": model_with_default_params.get_best_iteration(),
-            "allow_writing_files": False,
-            "verbose": True,
+    model = lgb.train(
+        {
+            "n_estimators": model_with_default_params.num_trees(),
+            "objective": "binary",
+            "metric": "binary_logloss",
         },
+        train_data,
     )
 
-    y_pred = model.predict(test_data, prediction_type="Probability")[:, 1]
-    y_test = test_data.get_label()
+    y_pred = model.predict(test_data.data)
+    y_test: np.ndarray = test_data.label.values  # type: ignore
 
     results: Dict[int, Dict[str, float]] = {}
     for k in k_values:
-        test_ndcg = binary_ndcg_at_k(y_pred, y_test, n_items=100, k=k)
-        test_map = map_at_k(y_pred, y_test, n_items=100, k=k)
-        test_hr = hit_ratio_at_k(y_pred, y_test, n_items=100, k=k)
+        test_ndcg = binary_ndcg_at_k(y_pred, y_test, n_items=100, k=k)  # type: ignore
+        test_map = map_at_k(y_pred, y_test, n_items=100, k=k)  # type: ignore
+        test_hr = hit_ratio_at_k(y_pred, y_test, n_items=100, k=k)  # type: ignore
 
         results[k] = {
             "ndcg": test_ndcg,
@@ -103,4 +109,4 @@ def train_ctb_model_and_evaluate_ranking_metrics(
 
 
 if __name__ == "__main__":
-    train_ctb_model_and_evaluate_ranking_metrics()
+    train_lgb_model_and_evaluate_ranking_metrics()
